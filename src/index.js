b2c87635cf686148f1ca04fc353a4bca9d1a423d
Mathew Kamkar <578302+matkam@users.noreply.github.com>
2021-11-05 16:14:57 -0700

    multiple tabs

--- src/index.js
+++ src/index.js
@@ -6,28 +6,24 @@ const lkapi = require('livekit-server-sdk');
   if (!identityPrefix) {
     identityPrefix = "tester"
   }
+
   let roomName = process.env.LIVEKIT_ROOM
+
   let testerMinutes = parseInt(process.env.DURATION)
   if (!testerMinutes) {
     testerMinutes = 30
   }
+
   let enablePublish = 0
-  if (process.env.LIVEKIT_ENABLE_PUB) {
+  if (process.env.ENABLE_PUBLISH) {
     enablePublish = 1
   }
 
-  const identity = `${identityPrefix}${Math.floor(Math.random() * 10000)}`
+  let tabCount = parseInt(process.env.TABS)
+    if (!tabCount) {
+    tabCount = 1
+  }
 
-  const at = new lkapi.AccessToken(undefined, undefined, {
-    identity: identity,
-    ttl: '1h',
-  });
-  at.addGrant({
-    room: roomName,
-    roomJoin: true,
-  })
-
-  const url = `https://example.livekit.io/#/room?url=${encodeURIComponent(process.env.LIVEKIT_HOST)}&token=${at.toJwt()}&videoEnabled=${enablePublish}&audioEnabled=${enablePublish}&simulcast=${enablePublish}`
   const browser = await puppeteer.launch({
     headless: true,
     args: [
@@ -40,12 +36,27 @@ const lkapi = require('livekit-server-sdk');
     ],
     ignoreDefaultArgs: ['--mute-audio']
   });
-  const page = await browser.newPage();
-  await page.setViewport({
-    width: 1000,
-    height: 700
-  });
-  await page.goto(url, {waitUntil: 'load'});
+
+  for (var i = 0; i < tabCount; i++) {
+    const identity = `${identityPrefix}${Math.floor(Math.random() * 10000)}`
+
+    const at = new lkapi.AccessToken(undefined, undefined, {
+      identity: identity,
+      ttl: '1h',
+    });
+    at.addGrant({
+      room: roomName,
+      roomJoin: true,
+    })
+
+    const url = `https://example.livekit.io/#/room?url=${encodeURIComponent(process.env.LIVEKIT_HOST)}&token=${at.toJwt()}&videoEnabled=${enablePublish}&audioEnabled=${enablePublish}&simulcast=${enablePublish}`
+    const page = await browser.newPage();
+    await page.setViewport({
+      width: 1000,
+      height: 700
+    });
+    await page.goto(url, {waitUntil: 'load'});
+  }
 
   await sleep((testerMinutes + Math.random()) * 60 * 1000);
   await browser.close();
