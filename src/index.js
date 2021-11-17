fa5b09bd1aa4629a7333d6463d98222028e33c85
Mathew Kamkar <578302+matkam@users.noreply.github.com>
2021-11-17 09:26:44 -0800

    reduce more logs

--- src/index.js
+++ src/index.js
@@ -56,8 +56,8 @@ const lkapi = require('livekit-server-sdk');
       .on('console', message =>
         console.log(`${message.type().substr(0, 3).toUpperCase()} ${message.text()}`))
       .on('pageerror', ({ message }) => console.log(message))
-      .on('response', response =>
-        console.log(`${response.status()} ${response.url()}`))
+      // .on('response', response =>
+      //   console.log(`${response.status()} ${response.url()}`))
       .on('requestfailed', request =>
         console.log(`${request.failure().errorText} ${request.url()}`))
       
